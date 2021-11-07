57275b0b7dc7f95289d79e29584b11ca66a1dfa1
Gompei <shoueigonpei6028@gmail.com>
2021-11-07 10:37:47 +0900

    レコード追加

--- cloud_dns.tf
+++ cloud_dns.tf
@@ -4,3 +4,12 @@ resource "google_dns_managed_zone" "google_dns_managed_zone" {
   description = "my-gcp-domain-zone"
   visibility  = "public"
 }
+
+resource "google_dns_record_set" "rough_frontend" {
+  name = "rough.${google_dns_managed_zone.google_dns_managed_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = google_dns_managed_zone.google_dns_managed_zone.name
+  rrdatas = ["199.36.158.100"]
+}
